5474c6b2074d5b30fc473fc9a27f90a33802b76f
Muath Alzahrani <itsmuath@gmail.com>
2019-04-12 23:25:26 +0300

    Refactor app.js file

--- js/app.js
+++ js/app.js
@@ -1,5 +1,13 @@
 // Create a list that holds all the cards
-const allCards = ["fa fa-diamond", "fa fa-diamond", "fa fa-paper-plane-o", "fa fa-paper-plane-o", "fa fa-anchor", "fa fa-anchor", "fa fa-bolt", "fa fa-bolt", "fa fa-cube", "fa fa-cube", "fa fa-leaf", "fa fa-leaf", "fa fa-bicycle", "fa fa-bicycle", "fa fa-bomb", "fa fa-bomb"]
+const allCards = ["fa fa-diamond", "fa fa-diamond",
+    "fa fa-paper-plane-o", "fa fa-paper-plane-o",
+    "fa fa-anchor", "fa fa-anchor",
+    "fa fa-bolt", "fa fa-bolt",
+    "fa fa-cube", "fa fa-cube",
+    "fa fa-leaf", "fa fa-leaf",
+    "fa fa-bicycle", "fa fa-bicycle",
+    "fa fa-bomb", "fa fa-bomb"
+]
 
 // Specify the container of all cards
 const cardsContainer = document.querySelector(".deck");
@@ -8,11 +16,22 @@ const cardsContainer = document.querySelector(".deck");
 let ClickedCards = [];
 let matchedCount = 0;
 
-for (let i = 0; i < allCards.length; i++) { // Create the cards one by one
-    const card = document.createElement("li");
-    cardsContainer.appendChild(card);
-    card.classList.add("card");
-    card.innerHTML = `<i class="${allCards[i]}"></i>`;
+init();
+
+// Start the game
+function init() {
+    for (let i = 0; i < allCards.length; i++) { // Create the cards one by one
+        const card = document.createElement("li");
+        cardsContainer.appendChild(card);
+        card.classList.add("card");
+        card.innerHTML = `<i class="${allCards[i]}"></i>`;
+
+        // Add click event for each card
+        viewCard(card);
+    }
+}
+
+function viewCard(card) {
 
     // Add event listeners for clicking on the cards
     card.addEventListener("click", function () {
@@ -28,24 +47,8 @@ for (let i = 0; i < allCards.length; i++) { // Create the cards one by one
         if (ClickedCards.length === 1) { // If a single card has been clicked already
 
 
-            if (currentCard.innerHTML === previousCard.innerHTML) { // If the cards match:
-                // Add the matching card style to them and leave them open
-                previousCard.classList.add("match");
-                currentCard.classList.add("match");
-
-                // Increment the counter of matched cards to end the game when all cards are open
-                matchedCount += 2;
-
-                // Call gameEnd to end the game if all cards are open
-                gameEnd();
-
-            } else { // If the two cards don't match:
-                setTimeout(function () { // Flip the cards back after 500 milliseconds
-                    previousCard.classList.remove("open", "show");
-                    currentCard.classList.remove("open", "show");
-
-                }, 500);
-            }
+            // Compare both opened cards to check if they match
+            compare(currentCard, previousCard);
 
             // reset the clickedCards list to be used for next matchings
             ClickedCards = [];
@@ -55,9 +58,31 @@ for (let i = 0; i < allCards.length; i++) { // Create the cards one by one
     });
 }
 
+function compare(currentCard, previousCard) {
+    if (currentCard.innerHTML === previousCard.innerHTML) { // If the cards match:
+        // Add the matching card style to them and leave them open
+        previousCard.classList.add("match");
+        currentCard.classList.add("match");
+
+        // Increment the counter of matched cards to end the game when all cards are open
+        matchedCount += 2;
+
+        // Call endGame to end the game if all cards are open
+        endGame();
+
+    } else { // If the two cards don't match:
+        setTimeout(function () { // Flip the cards back after 500 milliseconds
+                    previousCard.classList.remove("open", "show");
+                    currentCard.classList.remove("open", "show");
+
+        }, 500);
+    }
+}
+
 // Shuffle function from http://stackoverflow.com/a/2450976
 function shuffle(array) {
-    var currentIndex = array.length, temporaryValue, randomIndex;
+    var currentIndex = array.length,
+        temporaryValue, randomIndex;
 
     while (currentIndex !== 0) {
         randomIndex = Math.floor(Math.random() * currentIndex);
@@ -71,31 +96,9 @@ function shuffle(array) {
 }
 
 // Check if all cards are open, then show a congrats message
-function gameEnd() {
+function endGame() {
     if (matchedCount === allCards.length)
         setTimeout(function () {
-            alert("Congratulations! You solved the game");
+            alert("Congratulations! You solved the game 😃");
         }, 200)
 }
-
-
-
-
-
-/*
- * Display the cards on the page
- *   - shuffle the list of cards using the provided "shuffle" method below
- *   - loop through each card and create its HTML
- *   - add each card's HTML to the page
- */
-
-/*
- * set up the event listener for a card. If a card is clicked:
- *  - display the card's symbol (put this functionality in another function that you call from this one)
- *  - add the card to a *list* of "open" cards (put this functionality in another function that you call from this one)
- *  - if the list already has another card, check to see if the two cards match
- *    + if the cards do match, lock the cards in the open position (put this functionality in another function that you call from this one)
- *    + if the cards do not match, remove the cards from the list and hide the card's symbol (put this functionality in another function that you call from this one)
- *    + increment the move counter and display it on the page (put this functionality in another function that you call from this one)
- *    + if all cards have matched, display a message with the final score (put this functionality in another function that you call from this one)
- */
